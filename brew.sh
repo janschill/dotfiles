2a13ed31b160c194774beb98b271e047aa73f771
Jan Schill <janschill@Jans-MacBook-Pro.local>
2020-01-27 23:04:21 +0100

    first commit

---
+++ brew.sh
@@ -0,0 +1,55 @@
+#!/usr/bin/env bash
+
+# Install Homebrew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+brew update
+brew upgrade
+
+# Save Homebrew’s installed location.
+BREW_PREFIX=$(brew --prefix)
+
+# Install GNU core utilities (those that come with macOS are outdated).
+# Don’t forget to add `$(brew --prefix coreutils)/libexec/gnubin` to `$PATH`.
+brew install coreutils
+ln -s "${BREW_PREFIX}/bin/gsha256sum" "${BREW_PREFIX}/bin/sha256sum"
+
+# Install a modern version of Bash.
+brew install bash
+brew install bash-completion2
+
+# Switch to using brew-installed bash as default shell
+if ! fgrep -q "${BREW_PREFIX}/bin/bash" /etc/shells; then
+  echo "${BREW_PREFIX}/bin/bash" | sudo tee -a /etc/shells;
+  chsh -s "${BREW_PREFIX}/bin/bash";
+fi;
+
+# Install more recent versions of some macOS tools.
+brew install vim --with-override-system-vi
+brew install grep
+brew install openssh
+brew install screen
+brew install php
+brew install gmp
+
+# Install some useful tool
+brew install ack
+brew install git
+
+# Install some useful casks
+brew cask install alfred
+brew cask install dash
+brew cask install docker
+brew cask install wireshark
+brew cask install enpass
+brew cask install google-chrome
+brew cask install rectangle
+brew cask install slack
+brew cask install spotify
+brew cask install telegram
+brew cask install transmission
+brew cask install visual-studio-code
+brew cask install vlc
+brew cask install whatsapp
+
+brew cleanup
